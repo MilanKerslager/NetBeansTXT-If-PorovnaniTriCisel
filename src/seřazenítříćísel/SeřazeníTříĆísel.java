a661463102693f67e4b0a49382cc803c316ca8eb
ke <ke@B110-17.pslib.cz>
2015-09-30 09:09:31 +0200

    přidán vstup tří čísel z klávesnice

--- src/seřazenítříćísel/SeřazeníTříĆísel.java
+++ src/seřazenítříćísel/SeřazeníTříĆísel.java
@@ -6,13 +6,18 @@
 
 package seřazenítříćísel;
 
+import java.util.Scanner;
+
 public class SeřazeníTříĆísel {
 
     public static void main(String[] args) {
-        int a = 3;
-        int b = 2;
-        int c = 1;
-        
+        Scanner vstup = new Scanner(System.in);
+        System.out.print("Zadej 1. číslo: ");
+        int a = vstup.nextInt();
+        System.out.print("Zadej 2. číslo: ");
+        int b = vstup.nextInt();
+        System.out.print("Zadej 3. číslo: ");
+        int c = vstup.nextInt();
         if (a>b) {
             // prohození obsahu proměnných a,b
             int pom = a;
